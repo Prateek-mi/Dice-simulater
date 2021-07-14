bcee9210151d3dd0cf93b80541253bfb16a8900f
Prateek-mi <85389648+Prateek-mi@users.noreply.github.com>
2021-07-14 23:03:40 +0530

    Add files via upload
    File import from computer

---
+++ dicesimulator.py
@@ -0,0 +1,25 @@
+#dice simulator
+#rock paper scissor
+#vocab notifier
+#image editor
+#desktop assistant
+import random
+
+def diceing():
+ tries = input('LETS PLAY - say dice:  ')
+ while True:
+     if tries == 'dice':
+         dice = random.randint(1,6)
+         print(f"you got {dice}")
+         tries = input("play more - say 'dice' to play OR -> 'exit' to quit the game")
+         if tries == 'exit':
+             break
+         elif tries == 'dice':
+             pass
+         else:
+            pass
+
+     else:
+         print('type corectly please')
+         return diceing()
+diceing()
